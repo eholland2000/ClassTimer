ef9aa932f3b3bfb55b02be0dd3fc051f10b01005
cameronanundson <52716849+cameronanundson@users.noreply.github.com>
2020-07-23 16:43:00 -0500

    Created constants.js

---
+++ constants.js
@@ -0,0 +1,11 @@
+const ACTIVITY_TIMES = {
+   INDEPENDENT:"PT10M",
+   GROUP:"PT15M",
+   LONG_GROUP:"PT25M",
+   HOMEWORK:"PT1H",
+   TRANSITION:"PT3M"
+};
+
+module.exports = {
+   ACTIVITY_TIMES
+};
